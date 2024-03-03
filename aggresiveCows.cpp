b503428ccd9206616923a01931d1aae33bfc9eee
Amandeep Mishra <68015433+hrithik712@users.noreply.github.com>
2024-03-03 09:16:08 +0530

    Add files via upload

---
+++ aggresiveCows.cpp
@@ -0,0 +1,50 @@
+bool isPossible(vector <int> &stalls, int mid, int k)
+{
+    int cowCount = 1;
+    int lastPos = stalls[0];
+
+    for(int i=0;i<stalls.size();i++)
+    {
+        if(stalls[i] - lastPos >= mid)
+        {
+            cowCount++;
+            if(cowCount == k)
+            {
+                return true;
+            }
+            lastPos = stalls[i];
+        }
+    }
+
+    return false;
+
+}
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+    int s = 0;
+    int ans  =  -1;
+
+    int maxi = -1;
+    for(int i=0;i<stalls.size();i++) {
+      maxi = max(maxi, stalls[i]);
+    }
+    int e = maxi;
+    int mid = s+(e-s)/2;
+
+    while(s<=e)
+    {
+        if(isPossible(stalls, mid, k))
+        {
+            ans = mid;
+            s = mid+1;
+        }
+        else
+        {
+            e = mid-1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+    
