7a8141f0a5155b6dc8dd65f29a8e33dcd13a2316
Amandeep Mishra <68015433+hrithik712@users.noreply.github.com>
2024-03-18 12:00:41 +0530

    Add files via upload

---
+++ mergeSortedArray.cpp
@@ -0,0 +1,24 @@
+// nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while(j>= 0)
+        {
+            if(i >= 0  && nums1[i] > nums2[j])
+            {
+                nums1[k--] = nums1[i--];
+            }
+            else
+            {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        
+    }
+};
