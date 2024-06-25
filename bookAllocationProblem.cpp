e66913ee74a63e55f63218aa8bd010931a117583
Amandeep Mishra <68015433+hrithik712@users.noreply.github.com>
2024-06-25 23:41:18 +0530

    Add files via upload

--- bookAllocationProblem.cpp
+++ bookAllocationProblem.cpp
@@ -1,54 +1,50 @@
-#include<vector>
-using namespace std;
-
-bool isPossible(vector<int> arr, int n, int m, int mid) {
-    int studentCount = 1;
-    int pageSum = 0;
-    //cout << "checking for mid "<< mid <<endl;
-    
-    for(int i = 0; i<n; i++) {
-        if(pageSum + arr[i] <= mid) {
-            pageSum += arr[i];
-        }
-        else
+class Solution {
+  public:
+  bool isPossibleSolution(int arr[], int n, int m, int sol){
+      int pageSum =0;
+      int c = 1;
+      for(int i=0; i<n; i++){
+          if(arr[i] > sol){
+              return false;
+          }
+          if(pageSum + arr[i] > sol){
+              c++;
+              pageSum = arr[i];
+              if(c > m){
+                  return false;
+              }
+          }
+          else{
+              pageSum += arr[i];
+          }
+      }
+      return true;
+  }
+    // Function to find minimum number of pages.
+    long long findPages(int n, int arr[], int m) {
+        // code here
+        if(m>n) 
+        return -1;
+        
+        int start=0;
+        int end = accumulate(arr, arr+n, 0);
+        int ans = -1;
+        
+        while(start<=end)
         {
-            studentCount++;
-            if(studentCount > m || arr[i] > mid ) {
-            return false;
+            int mid = (start+end) >> 1;
+            if(isPossibleSolution(arr, n, m, mid))
+            {
+                ans = mid;
+                end = mid -1;
+                
+            }
+            else
+            {
+                start = mid+1;
+            }
+            
         }
-            pageSum = arr[i];
-        }
-        if(studentCount > m) {
-            return false;
-        }
-        //cout << " for i " << i << " Student "<< studentCount << " pageSum " << pageSum << endl;
+        return ans;
     }
-    return true;
-}
-
-int allocateBooks(vector<int> arr, int n, int m) {
-    int s = 0;
-    int sum = 0;
-    
-    for(int i = 0; i<n; i++) {
-        sum += arr[i];
-    }
-    int e = sum;
-    int ans = -1;
-    int mid = s + (e-s)/2;
-    
-    while(s<=e)
-    {
-        if(isPossible(arr,n,m,mid)) {
-            //cout<<" Mid returned TRUE" << endl;
-            ans = mid;
-            e = mid - 1;
-        }
-        else
-        {
-            s = mid + 1;
-        }
-        mid = s + (e-s)/2;
-    }
-    return ans;
-}
+};
